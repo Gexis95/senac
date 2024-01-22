8140b94d5df8bbc09cf84c00190b97957c0f73c7
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2024-01-22 17:27:37 -0300

    22/01/2024
    Feito o botão para ir na próxima página,
    página 2 pronta com botão esperando a criação da terceira página

--- PROJETOS/android projetos/navegacao/lib/main.dart
+++ PROJETOS/android projetos/navegacao/lib/main.dart
@@ -11,7 +11,7 @@ class Aplicativo extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Pagina1(),
+      home: Pagina1()
     );
   }
 }
@@ -19,28 +19,102 @@ class Aplicativo extends StatelessWidget {
 class Pagina1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //fornece as estruturas basicas
+    return Scaffold(
+      //fornece as estruturas basicas
 
-      appBar: AppBar(//barra de cima
+      appBar: AppBar(
+        //barra de cima
 
         //texto e cor da barra
         title: const Text(
           'Página 1',
-           style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.white)
         ),
-        backgroundColor: Colors.blue,//cor da barra
+        backgroundColor: Colors.blue, //cor da barra
       ),
 
-      body: Center(   //corpo da aplicação
+      body: Center(
+        //corpo da aplicação
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network('https://miro.medium.com/v2/resize:fit:578/1*szmA2b5DQS_U5wAGgHiPQg.png', width: 300, height: 300,),
-            const Text('Bem-Vindo a tela Inicial'),
+            Image.network(
+              'https://miro.medium.com/v2/resize:fit:578/1*szmA2b5DQS_U5wAGgHiPQg.png',
+              width: 300,
+              height: 300
+            ),
+            const Text(
+              'Bem-Vindo a tela Inicial',
+              style: TextStyle(fontSize: 30)
+            ),
+            const Text(
+              'Flutter é uma ferramenta multiplataforma - Android e IOS com um único código',
+            ),
+            const SizedBox(
+              height: 20
+            ), //quebra de linha
+            ElevatedButton(
+              onPressed: () {
+                //Navigator, gerencia navegações entre telas
+                Navigator.push(
+                  //context identifica a pagina atual
+                  context,
+                  //MaterialPage define a animação e layout e usa o builder para aprensentar a próxima tela
+                  MaterialPageRoute(builder: (context)=> Pagina2())
+                );
+              },
+              child: const Text('Ir para Página 2')
+            ),
           ],
-        ),      
+        ),
       ),
-
     );
   }
+}
+
+class Pagina2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    appBar: AppBar(
+      title: const Text('Pagina 2'),
+      backgroundColor: Colors.green
+    ),
+
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://dart.dev/assets/shared/dart-logo-for-shares.png?2',
+            width: 400,
+            height: 400,
+          ),
+          const SizedBox(height: 20),
+
+          const Text(
+            'Linguagem DART',
+            style: TextStyle(fontSize: 30)
+          ),
+
+          const Text(
+            'É uma linguagem versátil que combina eficiência e desempenho'
+          ),
+          const SizedBox(height: 20),
+
+          ElevatedButton(onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context)=> Pagina3())
+                );
+          },
+          child: const Text('Ir para Página 3'),
+          ),
+        ],
+      ),
+    ),
+   );
+  }
+
+
 }
