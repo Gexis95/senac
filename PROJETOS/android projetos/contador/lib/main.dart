4a6e7c5770a4b033cd10861e8d9c78f1f562641f
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2024-01-26 17:28:53 -0300

    26/01/2024
    Adicionado o contador para os cliques (e funcionando), variaveis para o timer e cliques

--- PROJETOS/android projetos/contador/lib/main.dart
+++ PROJETOS/android projetos/contador/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +6,7 @@ void main() {
 }
 
 class Aplicativo extends StatefulWidget {
-  const Aplicativo({Key? key}) : super(key: key);
+  const Aplicativo({super.key});
 
   @override
   State<Aplicativo> createState() => _EstadoAplicativo();
@@ -13,16 +14,55 @@ class Aplicativo extends StatefulWidget {
 
 //"_" indica que a classe é privada
 class _EstadoAplicativo extends State<Aplicativo> {
+  int contador1 = 0;
+  int contador2 = 0;
+  var _tempo = 60;
+  late Timer _timer;
+  bool _clique = true;
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 33, 0, 79),
-          title: const Text('Jogo contador', style: TextStyle(color: Colors.white),
+          title: const Text(
+            'Jogo contador',
+            style: TextStyle(color: Colors.white),
           ),
+        ),
+
+        //corpo do aplicativo +  centralização
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Contador 1: $contador1',
+                style: const TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
+        ),
+
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 50.0),
+              child: FloatingActionButton(
+                //onPressed: _clique ? (){setState(() {});} :null,
+                onPressed: () {
+                  setState(() {
+                    contador1++;
+                  });
+                },
+                tooltip: 'Incrementar Contador 1',
+                child: const Icon(Icons.add),
+              ),
+            ),
+          ],
         ),
       ),
     );
