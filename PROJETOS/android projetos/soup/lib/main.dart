fda340a7c55a9820971e595810072277faeecd05
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2024-03-14 17:23:43 -0300

    14/03/2024
    projeto novo adicionamo coisa e corrigi uns treco

--- PROJETOS/android projetos/soup/lib/main.dart
+++ PROJETOS/android projetos/soup/lib/main.dart
@@ -25,8 +25,8 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Menu Principal',
-         style: TextStyle(color: Colors.white)),
+        title:
+            const Text('Menu Principal', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.indigo,
         centerTitle: true,
       ),
@@ -34,7 +34,41 @@ class Menu extends StatelessWidget {
         child: GridView.count(
           crossAxisCount: 3,
           children: <Widget>[
-            Botao(text: 'Tela1', route: '/tela1', icons: Icons.home),
+            botao(texto: 'Tela1', rota: '/tela1', icone: Icons.home),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class botao extends StatelessWidget {
+  final String texto;
+  final String rota;
+  final IconData icone;
+  const botao(
+      {Key? key, required this.texto, required this.rota, required this.icone})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0))),
+        onPressed: () {
+          Navigator.pushNamed(context, rota);
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icone, color: Colors.indigo, size: 50,),
+            SizedBox(height: 8),
+            Text(
+              texto,
+              style: TextStyle(color: Colors.black, fontSize: .0),
+            ),
           ],
         ),
       ),
