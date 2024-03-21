401e646baa0fae8412e9cac893f66ab6643ea3d0
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2024-03-21 17:28:24 -0300

    21/03/2024
    commits sobre os últimos dias. Não teve tanta coisa nova no código mas no geral só implementando a parte do front-end sobre o comparativo de combustível

--- PROJETOS/android projetos/soup/lib/Tela1.dart
+++ PROJETOS/android projetos/soup/lib/Tela1.dart
@@ -1,18 +1,64 @@
 import 'package:flutter/material.dart';
 
-class Tela1 extends StatelessWidget {
+class Tela1 extends StatefulWidget {
+  @override
+  calcularCombustivelEstado createState() => calcularCombustivelEstado();
+}
+
+class calcularCombustivelEstado extends State<Tela1> {
+  TextEditingController entradaAlcool = TextEditingController();
+  TextEditingController entradaGasolina = TextEditingController();
+  IconData icone = Icons.help;
+  Color corIcone = Colors.black;
+  String resultado = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tela 1'),
-        backgroundColor: Colors.blue,
+        title: const Text(
+          'Álcool ou Gasolina?',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.indigo,
       ),
-      body: const Center(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          children: <Widget>[
-            Text('Olá Tela 1'),
-            Icon(Icons.home, size: 100, color: Colors.blue),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: entradaAlcool,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Preço do Álcool'),
+            ),
+            
+            SizedBox(height: 20),
+            TextField(
+              controller: entradaGasolina,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Preço da Gasolina'),
+            ),
+
+            const SizedBox(height: 20),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Calcular'),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            Icon(icone, size: 100, color: corIcone),
+            const SizedBox(
+              height: 10,
+            ),
+
+            Text(
+              resultado,
+              textAlign: TextAlign.center,
+              style:const TextStyle(fontSize: 18),
+            ),
           ],
         ),
       ),
