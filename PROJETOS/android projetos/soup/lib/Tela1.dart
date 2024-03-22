9298ac5c9793845d00c2744c6f84f5cc3182ba6e
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2024-03-22 17:29:00 -0300

    22/03/2024
    criando a váriavel pra quando o valor da gasolina/alcool for 0

--- PROJETOS/android projetos/soup/lib/Tela1.dart
+++ PROJETOS/android projetos/soup/lib/Tela1.dart
@@ -6,11 +6,29 @@ class Tela1 extends StatefulWidget {
 }
 
 class calcularCombustivelEstado extends State<Tela1> {
+  //variaveis do app.
   TextEditingController entradaAlcool = TextEditingController();
   TextEditingController entradaGasolina = TextEditingController();
   IconData icone = Icons.help;
   Color corIcone = Colors.black;
   String resultado = '';
+
+  void calcularOpcao() {
+    double precoAlcool = double.tryParse(entradaAlcool.text) ?? 0.00;
+    double precoGasolina = double.tryParse(entradaGasolina.text) ?? 0.00;
+
+    //se o precoAlool = 0 ou precoGasolina = 0
+    if (precoAlcool == 0 || precoGasolina == 0) {
+      setState(() {
+        //troca o icone pra vermelho
+        icone = Icons.error;
+        corIcone = Colors.red;
+        //informa o usuário do erro cometido
+        resultado = 'Por favor insira o preço do alcool ou gasolina';a
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,33 +49,28 @@ class calcularCombustivelEstado extends State<Tela1> {
               keyboardType: TextInputType.number,
               decoration: const InputDecoration(labelText: 'Preço do Álcool'),
             ),
-            
             SizedBox(height: 20),
             TextField(
               controller: entradaGasolina,
               keyboardType: TextInputType.number,
               decoration: const InputDecoration(labelText: 'Preço da Gasolina'),
             ),
-
             const SizedBox(height: 20),
             const ElevatedButton(
               onPressed: null,
               child: Text('Calcular'),
             ),
-
             const SizedBox(
               height: 20,
             ),
-
             Icon(icone, size: 100, color: corIcone),
             const SizedBox(
               height: 10,
             ),
-
             Text(
               resultado,
               textAlign: TextAlign.center,
-              style:const TextStyle(fontSize: 18),
+              style: const TextStyle(fontSize: 18),
             ),
           ],
         ),
