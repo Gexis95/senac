d143eb10ac1546c85d249da170829bd04752e566
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2024-04-19 17:27:16 -0300

    19/04/2024
    Parte da gorjeta pronta, o código só está com problema, quinta eu resolvo B)

--- PROJETOS/android projetos/soup/lib/tela4.dart
+++ PROJETOS/android projetos/soup/lib/tela4.dart
@@ -6,21 +6,66 @@ class Tela4 extends StatefulWidget {
 }
 
 class calcular extends State<Tela4> {
+  double valor = 0.0;
+  int porcentagem = 10;
+
+  double calcularTotal() {
+    double qtdGorjeta = (valor * porcentagem) / 100;
+    double total = qtdGorjeta + valor;
+    return total;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 15, 16, 24),
       appBar: AppBar(
-        title: Text('Calculadora de Gorgeta', style: TextStyle(color: Colors.white),),
+        title: Text(
+          'Calculadora de Gorgeta',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.indigo,
       ),
-      body: Padding(padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[],
+      body: Padding(
+        padding:  EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Valor da Conta'),
+              onChanged: (value) {
+                setState(() {
+                  valor = double.tryParse(value) ?? 0;
+                });
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text('Porcentagem de Gorjeta: $porcentagem%', style: TextStyle(fontSize: 25)),
+                ),
+                Slider(
+                  value: porcentagem.toDouble(),
+                  min: 0,
+                  max: 50,
+                  divisions: 10,
+                  onChanged: (double value){
+                    setState(() {
+                      porcentagem = value.round();
+                    },);
+                  } ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30,),
+            Text('Total da Conta: ${calcularTotal()}', style: TextStyle(fontSize: 25),),
+          ],
+        ),
       ),
-      ),
-
     );
   }
 }
